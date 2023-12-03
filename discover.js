b031ad439ceac443b639765943bccc75e563dc85
d-deon <d.deonmusic@gmail.com>
2023-12-03 21:27:49 +0000

    website.remodelation_2

--- discover.js
+++ discover.js
@@ -3,26 +3,33 @@ document.addEventListener('DOMContentLoaded', function () {
     const clickSound = new Audio('audio/blop-sound.mp3');
 
     icons.forEach(icon => {
+        icon.setAttribute('data-state', 'original');
         icon.addEventListener('click', function () {
             const iconId = this.getAttribute('data-icon-id');
-            const currentImage = this.querySelector('img').src;
-            const newImageSrc = determineNewImage(iconId, currentImage);
+            const currentState = this.getAttribute('data-state');
+
+            const newState = currentState === 'original' ? 'new' : 'original';
+
+            const newImageSrc = determineNewImage(iconId, newState);
             this.querySelector('img').src = newImageSrc;
+
             clickSound.play();
+
+            this.setAttribute('data-state', newState);
         });
     });
 
-   
-    function determineNewImage(iconId, currentImage) {
+    function determineNewImage(iconId, state) {
+    
         switch (iconId) {
             case 'crosswords':
-                return 'images/discover_puzzles.jpg';
+                return state === 'original' ? 'images/discover_puzzles.jpg' : 'images/cross-med.png';
             case 'puns':
-                return 'images/discover_pelvis.png';
+                return state === 'original' ? 'images/discover_pelvis.png' : 'images/releases.small.png';
             case 'friends':
-                return 'images/discover_friends.jpg';
+                return state === 'original' ? 'images/discover_friends.jpg' : 'images/more-small.png';
             case 'music':
-                return 'images/guitar-pic-bw.jpg';
+                return state === 'original' ? 'images/guitar-pic-bw.jpg' : 'images/gigs-small.png';
             default:
                 return currentImage;
         }
